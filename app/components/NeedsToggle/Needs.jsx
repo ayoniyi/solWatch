0a898948984e822710a3f846337f4f4ec88d358a
ayoniyi <ayo.a.niyi@gmail.com>
2024-06-13 23:56:36 -0700

    update: add resp

--- app/components/NeedsToggle/Needs.jsx
+++ app/components/NeedsToggle/Needs.jsx
@@ -5,11 +5,14 @@ import style from "./Need.module.scss";
 const NeedsToggle = () => {
   useEffect(() => {
     const accordion = document.getElementsByClassName(style.need);
-
     let i;
     for (i = 0; i < accordion.length; i++) {
       accordion[i].addEventListener("click", function () {
-        this.classList.toggle(style.activeAcc);
+        if (this.classList.contains(style.activeAcc)) {
+          this.classList.remove(style.activeAcc);
+        } else {
+          this.classList.add(style.activeAcc);
+        }
       });
     }
   }, []);
@@ -17,7 +20,7 @@ const NeedsToggle = () => {
     <>
       <div className={style.container}>
         <div className={style.needs}>
-          <div nclassName={style.need}>
+          {/* <div nclassName={style.need}>
             <div className={style.faqLabel}>
               <div className={style.needNumber}>
                 <p>1</p>
@@ -33,7 +36,24 @@ const NeedsToggle = () => {
                 the AI.
               </p>
             </div>
-          </div>
+          </div> */}
+          {/* <div nclassName={style.need}>
+            <div className={style.faqLabel}>
+              <div className={style.needNumber}>
+                <p>1</p>
+              </div>
+              <div className={style.needTitle}>
+                <h3>Trading AI Assistant</h3>
+              </div>
+            </div>
+            <div className={style.faqContentsm}>
+              <p>
+                Voice-controlled AI trading assistant, easily set price alerts,
+                establish trading plans, making trading operations simpler with
+                the AI.
+              </p>
+            </div>
+          </div> */}
           {/* <div className={style.need}>
             <div className={style.needTop}>
               <div className={style.needNumber}>
